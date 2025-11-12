f5554079cebe6b05572fd044a3aca4dbc43cb813
Arseniy <senya57k@gmail.com>
2025-11-12 23:34:15 +0300

    add front for employees list

--- src/app/employe-list.component/employe-list.component.ts
+++ src/app/employe-list.component/employe-list.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+interface Employee {
+  name: string;
+  last_name: string;
+  middle_name: string;
+  organization: string;
+}
 
 @Component({
   selector: 'app-employe-list.component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './employe-list.component.html',
   styleUrl: './employe-list.component.css',
 })
-export class EmployeListComponent {
+export class EmployeListComponent implements OnInit, OnDestroy {
+  employees: Employee[] = [];
+
+
+  ngOnInit(): void {
+    this.loadEmployees();
+  }
+
+  ngOnDestroy(): void {
+    this.employees = [];
+  }
+
+  loadEmployees() {
+    this.employees = [
+      {
+        name: 'Azazin',
+        last_name: 'Creed',
+        middle_name: 'Creedovich',
+        organization: 'Sber'
+      },
+      {
+        name: 'Azazin2',
+        last_name: 'Creed',
+        middle_name: 'Creedovich',
+        organization: 'Sber'
+      },
+      {
+        name: 'Azazin3',
+        last_name: 'Creed',
+        middle_name: 'Creedovich',
+        organization: 'Sber'
+      }
+    ]
+  }
 
 }
