92a8c2756a885144b10490c031ca4e9c765fba44
Arseniy Kuzenkov <senya57k@gmail.com>
2026-03-08 17:35:54 +0300

    add patch method for updating coures lessons

--- backend/routes/course.routes.js
+++ backend/routes/course.routes.js
@@ -13,6 +13,8 @@ router.post('/courses/:id/lessons', courseController.addLessonToCourse)
 // Partial update of course (mark, conclusion, etc.)
 router.patch('/courses/:id', courseController.updateCourse)
 
+router.patch('/courses/:course_id/lessons/:lesson_id', courseController.updateLesson)
+
 router.delete('/courses/:course_id/lessons/:lesson_id', courseController.deleteLessonFromCourse)
 
 module.exports = router
